bb4584dacc85e5f1c34229f2afca379db518d407
flipott <ottphillipa@gmail.com>
2022-12-16 15:36:17 -0500

    Clean up code

--- models/author.js
+++ models/author.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
 
 const AuthorSchema = new Schema({
     first_name: { type: String, required: true, maxLength: 100 },
@@ -22,5 +23,20 @@ AuthorSchema.virtual("name").get(function() {
 AuthorSchema.virtual("url").get(function() {
     return `/catalog/author/${this._id}`;
 });
+
+AuthorSchema.virtual("date_formatted").get(function () {
+    if (this.date_of_birth && this.date_of_death) {
+        const birthStr = DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
+        console.log(birthStr);
+        const deathStr = DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED);
+        return `${birthStr} - ${deathStr}`
+    } else if (this.date_of_birth && !this.date_of_death) {
+        const birthStr = DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
+        return `${birthStr}`
+    } else {
+        return 'Date of birth unknown';
+    }
+})
+
 
 module.exports = mongoose.model("Author", AuthorSchema);
