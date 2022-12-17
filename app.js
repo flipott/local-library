28225cf1fdc3c0ed9bcc60e9c1254003b3542637
flipott <ottphillipa@gmail.com>
2022-12-16 19:00:47 -0500

    Add files and changes required for deployment

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ var app = express();
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB = "mongodb+srv://admin:i3PL6vMuiFdhKXBX@cluster0.ml00m5a.mongodb.net/local_library?retryWrites=true&w=majority"
+const dev_db_url = "mongodb+srv://admin:i3PL6vMuiFdhKXBX@cluster0.ml00m5a.mongodb.net/local_library?retryWrites=true&w=majority"
+const mongoDB = MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
